cbf589d0a4a025eb0528980740b4ecbf71343c04
Ikbal Kat <ikbalkatili307@gmail.com>
2026-01-30 19:19:55 +0800

    feat(file): materi 01 aturan penamaan

---
+++ 01_aturan_penamaan.py
@@ -0,0 +1,8 @@
+x = 165 #penamaan yang buruk
+tinggi_developer_cm = 165 #penamaan yang baik karena jelas dan deskriptif serta menggunakan snake_case
+
+"""Wajib Standar PEP 8 dimana 01_python itu adalah penamaan variable yang buruk, biasakan gunakan python_01
+Case Sensitive artinya besar kecil huruf itu berbeda, misal: namaDepan dan namadepan itu berbeda
+Jangan Gunakan Kata Kunci Python sebagai penamaan variable, misal: print, input, str, int, dll
+Jangan Gunakan Spasi pada penamaan variable, gunakan _ (underscore) sebagai pengganti spasi
+Jangan Gunakan karakter khusus pada penamaan variable, misal: nama@depan, nama#depan, dll"""
