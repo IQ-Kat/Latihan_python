bb3d325af10211da4d1849c4ce306dd450ca789b
Ikbal Kat <ikbalkatili307@gmail.com>
2026-01-30 19:32:30 +0800

    feat(file): materi 02 tipe data numerik(int & float)

---
+++ 02_tipe_data_numerik.py
@@ -0,0 +1,10 @@
+tipe_data_integer = 10
+tipe_data_float = 10.5
+tipe_data_complex = 5j # j adalah bagian dari bilangan kompleks di python, bukan i seperti di matematika pada umumnya, aka di pelajari di tingkat lanjut
+print("tipe_data_integer : ", tipe_data_integer, ", type : ", type(tipe_data_integer))
+print("tipe_data_float : ", tipe_data_float, ", type : ", type(tipe_data_float))
+print("tipe_data_complex : ", tipe_data_complex, ", type : ", type(tipe_data_complex))
+
+"""Penjumlahan Tipe Data Berbeda, int + float = float"""
+hasil_penjumlahan = tipe_data_integer + tipe_data_float
+print("hasil_penjumlahan (int + float) : ", hasil_penjumlahan, ", type : ", type(hasil_penjumlahan))
