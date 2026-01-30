be02aba32517284c07c5d712ba5f16e62e3d2e48
Ikbal Kat <ikbalkatili307@gmail.com>
2026-01-30 19:02:07 +0800

    feat(code): mengubah fungsi main.py + mulai menyusun materi per 1 file di mulai  dari 00_Comentar.py

---
+++ 00_Comentar.py
@@ -0,0 +1,3 @@
+print ("Komentar 1 Baris di Python Gunakan #")  # Ini adalah komentar satu baris, gunakan comentar hanya untuk kenapa atau mengapa jangan untuk apa
+print ("Komentar 2 Baris di Python Gunakan 3 kutip 2 kali") 
+"""Halo ini adalah Komentar Multi-baris (Docstring) yang bisa digunakan untuk Penjelasan Panjang Lebar, Jangan Letakan di samping Kode karena akan menimbulkan error"""
--- main.py
+++
@@ -1,7 +0,0 @@
-daftar_usia = (10, 20, 30, 40, 50)
-daftar_usia_list = list(daftar_usia)
-daftar_usia_list[0] = 15
-daftar_usia = tuple(daftar_usia_list)
-
-print(daftar_usia)
-print(type(daftar_usia))
